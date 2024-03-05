054bf684fac701fc502fa30a8a451767c2e0bb23
Volodymyr_D <dymyr307@gmail.com>
2024-03-05 22:32:21 +0200

    finally

--- modal.js
+++ modal.js
@@ -1,5 +1,15 @@
+const btn1 = document.getElementById('openModal_1');
+btn1.addEventListener('click', () => {
+  new Modal('Text for modal 1');
+});
+
+const btn2 = document.getElementById('openModal_2');
+btn2.addEventListener('click', () => {
+  new Modal('Text for modal <--------2------------>');
+});
+
 class Modal {
-  constructor(text) {
+  constructor(text = 'ERROR') {
     this.text = text;
     this.init();
   }
@@ -7,6 +17,8 @@ class Modal {
   init() {
     this.createMarkup();
     this.modal = document.getElementById('myModal');
+    this.closeBtn = this.modal.querySelector('.close');
+    this.attachEvents();
   }
 
   createMarkup() {
@@ -23,5 +35,28 @@ class Modal {
     );
   }
 
-  attachEvents() {}
+  attachEvents() {
+    this.closeFn = this.closeFn.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+
+    this.closeBtn.addEventListener('click', this.closeFn);
+    window.addEventListener('click', this.handleClick);
+  }
+
+  handleClick(e) {
+    if (e.target === this.modal) {
+      this.closeFn();
+    }
+  }
+
+  closeFn() {
+    this.detachEvents();
+    this.modal.remove();
+    this.modal = null;
+  }
+
+  detachEvents() {
+    this.closeBtn.removeEventListener('click', this.closeFn);
+    window.removeEventListener('click', this.handleClick);
+  }
 }
